efcf3bd1dbc5244e631a218c3dc455b2a0be55e1
Bruno T. Araujo <brunotda@gmail.com>
2020-09-23 17:47:07 -0300

    Reset base com gitcorreto

--- node/index.ts
+++ node/index.ts
@@ -1,40 +1,61 @@
 import {
-  LRUCache,
   Service,
-  ServiceContext,
+  IOClients,
   ParamsContext,
+  ServiceContext,
   RecorderState,
-  method,
 } from '@vtex/api'
-import { Clients } from './clients'
-import { analytics } from './handlers/analytics'
+import { example } from './events/example'
+import { createSendEvent } from './routes/notify'
+import { getCacheContext, setCacheContext } from './utils/cachedContext'
 
-// Create a LRU memory cache for the Status client.
-// The @vtex/api HttpClient respects Cache-Control headers and uses the provided cache.
-const memoryCache = new LRUCache<string, any>({ max: 5000 })
-metrics.trackCache('status', memoryCache)
+const TREE_SECONDS_MS = 3 * 1000
+const CONCURRENCY = 10
 
 declare global {
-  type Context = ServiceContext<Clients, State>
+  type Context = ServiceContext<IOClients, State>
 
   interface State extends RecorderState {
     code: number
   }
 }
 
-export default new Service<Clients, State, ParamsContext>({
+function sendEventWithTimer() {
+  setInterval(function () {
+    const context = getCacheContext()
+    if (!context) {
+      console.log('no context in memory')
+      return
+    }
+    return createSendEvent(context)
+  }, 30000)
+  console.log('FIRED HERE')
+}
+
+sendEventWithTimer()
+
+export default new Service<IOClients, State, ParamsContext>({
   clients: {
-    implementation: Clients,
     options: {
-      default: {
-        retries: 2,
-        timeout: 10000,
+      events: {
+        exponentialTimeoutCoefficient: 2,
+        exponentialBackoffCoefficient: 2,
+        initialBackoffDelay: 50,
+        retries: 1,
+        timeout: TREE_SECONDS_MS,
+        concurrency: CONCURRENCY,
       },
     },
   },
+  events: {
+    example,
+  },
   routes: {
-    analytics: method({
-      GET: [analytics],
-    }),
+    hcheck: (ctx: any) => {
+      setCacheContext(ctx)
+      ctx.set('Cache-Control', 'no-cache')
+      ctx.status = 200
+      ctx.body = 'ok'
+    },
   },
 })
