fac295b23f30ba67749326615659be65eb061831
Bruno T. Araujo <brunotda@gmail.com>
2020-09-24 14:22:06 -0300

    Limpeza de base e primeiro commit

--- node/clients/analytics.ts
+++
@@ -1,9 +0,0 @@
-import { AppClient, InstanceOptions, IOContext } from '@vtex/api'
-
-export default class Analytics extends AppClient {
-    constructor(context: IOContext, options?: InstanceOptions) {
-        super('vtex.mocked-analytics@0.x', context, options)
-    }
-    public getLiveUsers(): Promise<LiveUsersProduct[]> {}
-}
-
